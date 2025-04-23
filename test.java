4eb3d1e809c19bf9776082ada5044ebf212071a6
Ranjitrevanakar02@gmail.com <Ranjitrevanakar02@gmail.com>
2025-04-23 14:42:58 +0530

    save

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 4;
+
+        System.out.println(a + " " + b + " " + (a + b));
+        System.out.println(a + " " + b + " " + (a - b));
+    }
+}
