2c5fdd0a372c6ff1e35766a26ae8fdc79ab5e23d
Ranjitrevanakar02@gmail.com <Ranjitrevanakar02@gmail.com>
2025-05-07 15:18:34 +0530

    save

---
+++ java2/operation.java
@@ -0,0 +1,11 @@
+package java2;
+
+public class operation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("addition:" + (a + b));
+        System.out.println("addition:" + (a - b));
+        System.out.println("addition:" + (a * b));
+    }
+}
